ffa4fb5beba0818f394fdc2412080b2627fe0051
@azick99 <yunusaliyevazizbek80@gmail.com>
2023-01-09 13:06:45 +0100

    finish thunks

--- src/routes/categories-preview/CategoriesPreview.jsx
+++ src/routes/categories-preview/CategoriesPreview.jsx
@@ -1,16 +1,27 @@
-import { Fragment, } from 'react'
-import { useSelector } from 'react-redux'
-import CategoryPreview from '../../components/category-preview/CategoryPreview'
-import { categoriesSelector } from '../../store/categories/category.selector'
+import { Fragment } from "react"
+import { useSelector } from "react-redux"
+import CategoryPreview from "../../components/category-preview/CategoryPreview"
+import {
+  categoriesSelector,
+  selectIsCategoriesLoading,
+} from "../../store/categories/category.selector"
+import Spinner from "../../components/spinner/Spinner"
 
 const CategoriesPreview = () => {
   const categoriesMap = useSelector(categoriesSelector)
+  const isLoading = useSelector(selectIsCategoriesLoading)
   return (
     <Fragment>
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title]
-        return <CategoryPreview key={title} title={title} products={products} />
-      })}
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        Object.keys(categoriesMap).map((title) => {
+          const products = categoriesMap[title]
+          return (
+            <CategoryPreview key={title} title={title} products={products} />
+          )
+        })
+      )}
     </Fragment>
   )
 }
